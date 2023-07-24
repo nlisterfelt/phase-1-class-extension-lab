276f364c1d533b493f78b4eb6a089dbd093c722c
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-07-23 20:02:04 -0500

    square and triangle using inheritance

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Your code here
+class Polygon{
+    constructor(array){
+        this.array = array
+    }
+    get countSides(){
+        return this.array.length
+    }
+    get perimeter(){
+        let sum = 0
+        this.array.forEach(element=>sum+=element)
+        return sum
+    }
+}
+
+class Triangle extends Polygon{
+    get isValid(){
+        return this.array[0]+this.array[1]>this.array[2] && this.array[0]+this.array[2]>this.array[1] && this.array[1]+this.array[2]>this.array[0]  
+    }
+}
+
+class Square extends Polygon{
+    get isValid(){
+        return this.array[0]===this.array[1]&&this.array[1]===this.array[2]&&this.array[2]===this.array[3]
+    }
+    get area(){
+        if(this.isValid){
+            return this.array[0]**2
+        } else {
+            return 'not a square'
+        }
+    }
+}
+
+const mySquare = new Square([3,2,3,3])
+console.log(mySquare.isValid)
+console.log(mySquare.area)
